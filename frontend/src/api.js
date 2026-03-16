b79e55c3a765fb6bbf500f8a42b7be12cb8c1f8a
Aryan <chaubeyr189@gmail.com>
2026-03-16 21:47:24 +0530

    backend added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getWeatherPrediction = async (city) => {
 
     const response = await axios.get(
-        `http://192.168.0.163:10000/predict?city=${city}`
+        `https://weather-ai-predictor-wnxz.onrender.com/predict?city=${city}`
     );
 
     return response.data;
